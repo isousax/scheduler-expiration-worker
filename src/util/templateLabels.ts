ff5f1961c68620adfcb8d30a52f9b0b5e03f7aa0
Italo Williams <italowilliams2020@gmail.com>
2025-09-04 11:28:18 -0300

    adicionado novo label

--- src/util/templateLabels.ts
+++ src/util/templateLabels.ts
@@ -1,6 +1,7 @@
 const templateMap: Record<string, string> = {
     nossa_historia: "Nossa História",
-    infinito_particular: "Infinito Particular"
+    infinito_particular: "Infinito Particular",
+    bem_vindo_ao_mundo: "Bem-vindo ao Mundo"
 };
 
 export function templateLabels(templateId: string): string {
